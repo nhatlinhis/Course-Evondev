182a06f1488f1852b12d4c69e147949ea7db4127
nhatlinhis <nhatlinh91827@gmail.com>
2023-10-02 03:50:42 +0700

    Bai 90

--- src/components/youtube/Dropdown.js
+++ src/components/youtube/Dropdown.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Dropdown = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+  useEffect(() => {
+    function handleClickOutDropdown(e) {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        // console.log("Click outside dropdown");
+        setShowDropdown(false);
+      }
+      // } else {
+      //   console.log("Click inside");
+      // }
+    }
+    document.addEventListener("click", handleClickOutDropdown);
+    return () => {
+      document.removeEventListener("click", handleClickOutDropdown);
+    };
+  }, []);
+
   return (
-    <div className="relative w-full max-w-[400px] ">
-      <div className="p-5 border border-gray-200 rounded-lg w-full cursor-pointer">
+    <div className="relative w-full max-w-[400px] " ref={dropdownRef}>
+      <div
+        className="p-5 border border-gray-200 rounded-lg w-full cursor-pointer"
+        onClick={() => setShowDropdown(!showDropdown)}
+      >
         Selector
       </div>
-      <div className="border-gray-200 rounded-lg absolute top-full left-0 bg-white">
-        <div className="p-5">Angular</div>
-        <div className="p-5">ReactJS</div>
-        <div className="p-5">Vue</div>
-      </div>
+      {showDropdown && (
+        <div className="border-gray-200 rounded-lg absolute top-full left-0 bg-white">
+          <div className="p-5 cursor-pointer ">Angular</div>
+          <div className="p-5 cursor-pointer ">ReactJS</div>
+          <div className="p-5 cursor-pointer ">Vue</div>
+        </div>
+      )}
     </div>
   );
 };
