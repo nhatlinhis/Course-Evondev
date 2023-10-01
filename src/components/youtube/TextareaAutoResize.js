a630dbb1147729a44d1842618a1ba0c986d4c3f8
nhatlinhis <nhatlinh91827@gmail.com>
2023-10-01 17:14:25 +0700

    Bai 88

--- src/components/youtube/TextareaAutoResize.js
+++ src/components/youtube/TextareaAutoResize.js
@@ -1,18 +1,32 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const TextareaAutoResize = () => {
   const [text, setText] = useState("demo");
   const textareaRef = useRef(null);
   const [textareaHeight, setTextareaHeight] = useState("auto");
-  const [textareaWrapperHeight, setTextareaWrapperHeight] = useState("auto");
+  // const [parentHeight, setParentHeight] = useState("auto");
+
+  const handleChange = (event) => {
+    // console.log(event.target.value);
+    // setParentHeight(`${textareaRef?.current?.scrollHeight}px`);
+    setTextareaHeight("auto");
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    setTextareaHeight(`${textareaRef?.current?.scrollHeight}px`);
+    // setParentHeight(`${textareaRef?.current?.scrollHeight}px`);
+  }, [text]);
+
   return (
-    <div className="p-5" style={{ height: textareaWrapperHeight }}>
+    <div className="p-5">
       <textarea
-        className="w-full max-w-[400px] p-5 rounded-lg border border-gray-300 resize-none focus:border-blue-400"
+        className="transition-all overflow-hidden w-full max-w-[400px] p-5 rounded-lg border border-gray-300 decoration-fuchsia-400 resize-none leading-normal"
         placeholder="Pleaese enter your content..."
         value={text}
         ref={textareaRef}
         style={{ height: textareaHeight }}
+        onChange={handleChange}
       ></textarea>
     </div>
   );
