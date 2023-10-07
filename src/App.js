95823be244b7d750473ffe9e9a21ba78dab25a94
nhatlinhis <nhatlinh91827@gmail.com>
2023-10-07 19:10:30 +0700

    Maintain 101

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import React from "react";
-import HackerNewsWidthHook from "./components/news/HackerNewsWithHook";
+// import HackerNewsWidthHook from "./components/news/HackerNewsWithHook";
+import Dropdown from "./components/youtube/Dropdown";
 
 function App() {
   return (
     <div>
-      <HackerNewsWidthHook></HackerNewsWidthHook>
+      {/* <HackerNewsWidthHook></HackerNewsWidthHook> */}
+      <Dropdown></Dropdown>
     </div>
   );
 }
--- src/components/youtube/Dropdown.js
+++ src/components/youtube/Dropdown.js
@@ -1,33 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
+import useClickOutSide from "../../hook/useClickOutSide";
 
 const Dropdown = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const dropdownRef = useRef(null);
-  useEffect(() => {
-    function handleClickOutDropdown(e) {
-      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-        // console.log("Click outside dropdown");
-        setShowDropdown(false);
-      }
-      // } else {
-      //   console.log("Click inside");
-      // }
-    }
-    document.addEventListener("click", handleClickOutDropdown);
-    return () => {
-      document.removeEventListener("click", handleClickOutDropdown);
-    };
-  }, []);
-
+  const { show, setShow, nodeRef } = useClickOutSide();
   return (
-    <div className="relative w-full max-w-[400px] " ref={dropdownRef}>
+    <div className="relative w-full max-w-[400px] " ref={nodeRef}>
       <div
         className="p-5 border border-gray-200 rounded-lg w-full cursor-pointer"
-        onClick={() => setShowDropdown(!showDropdown)}
+        onClick={() => setShow(!show)}
       >
         Selector
       </div>
-      {showDropdown && (
+      {show && (
         <div className="border-gray-200 rounded-lg absolute top-full left-0 bg-white">
           <div className="p-5 cursor-pointer ">Angular</div>
           <div className="p-5 cursor-pointer ">ReactJS</div>
