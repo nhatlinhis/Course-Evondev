fd4ec904fe67d19519d31bb0231766e15041d089
nhatlinhis <nhatlinh91827@gmail.com>
2023-10-07 19:58:18 +0700

    Done Chuong 6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.scss";
-import React from "react";
-// import HackerNewsWidthHook from "./components/news/HackerNewsWithHook";
+import React, { useState } from "react";
 import Dropdown from "./components/youtube/Dropdown";
+import SideBarMenu from "./components/SideBarMenu";
+import useClickOutSide from "./hook/useClickOutSide";
 
 function App() {
+  // const [show, setShow] = useState(false);
+  const { show, setShow, nodeRef } = useClickOutSide("button");
   return (
     <div>
-      {/* <HackerNewsWidthHook></HackerNewsWidthHook> */}
+      <button
+        onClick={() => setShow(true)}
+        className="inline-block m-3 p-3 rounded-lg text-white bg-green-400"
+      >
+        Show menu
+      </button>
+      <SideBarMenu show={show} ref={nodeRef}></SideBarMenu>
       <Dropdown></Dropdown>
     </div>
   );
