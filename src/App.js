fbc96f1b6cec094372171faf6b8919e82480efe7
nhatlinhis <nhatlinh91827@gmail.com>
2023-10-08 02:16:50 +0700

    Bai 107

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
+import React from "react";
 import "./App.scss";
-import React, { useState } from "react";
-import Dropdown from "./components/youtube/Dropdown";
-import SideBarMenu from "./components/SideBarMenu";
-import useClickOutSide from "./hook/useClickOutSide";
+import Input from "./components/form/Input";
 
-function App() {
-  const { show, setShow, nodeRef } = useClickOutSide("button");
+const App = () => {
   return (
     <div>
-      <button
-        onClick={() => setShow(true)}
-        className="inline-block m-3 p-3 rounded-lg text-white bg-green-400"
-      >
-        Show menu
-      </button>
-      <SideBarMenu show={show} ref={nodeRef}></SideBarMenu>
-      <Dropdown></Dropdown>
+      <Input></Input>
     </div>
   );
-}
+};
 
 export default App;
