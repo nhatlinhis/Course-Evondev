73921d7588c7825d0235a64dc1a42a40ecf11b6a
nhatlinhis <nhatlinh91827@gmail.com>
2023-10-13 02:40:55 +0700

    Bai 112

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.scss";
-import Form2 from "./components/form/Form2";
+import MovieSearchApp from "./components/youtube/MovieSearchApp";
 
 const App = () => {
   return (
     <div>
-      <Form2></Form2>
+      <MovieSearchApp></MovieSearchApp>
     </div>
   );
 };
---
+++ src/components/youtube/MovieSearchApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//305e809e770e39392a1a79bc909fdfc4
+// https://api/themoviedb.org/3/movie/550?api_key=305e809e770e39392a1a79bc909fdfc4
+
+const MovieSearchApp = () => {
+  return <div></div>;
+};
+
+export default MovieSearchApp;
